ae8ef66b0ba87d73a394ee34f65db9125204aec7
ma_tw <example@example.com>
2024-04-20 19:36:17 +0900

    update dijkstra

--- test/dijkstra.test.cpp
+++ test/dijkstra.test.cpp
@@ -1,11 +1,11 @@
-#define PROBLEM "https://judge.yosupo.jp/problem/shortest_path"
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_1_A"
 #include <bits/stdc++.h>
 #include "../graph/dijkstra.hpp"
 using namespace std;
 
 int main() {
-    int n, m, s, t;
-    cin >> n >> m >> s >> t;
+    int n, m, r;
+    cin >> n >> m >> r;
     vector<vector<Edge>> g(n), inv(n);
     for (int i = 0; i < m; i++) {
         int a, b, c;
@@ -13,24 +13,9 @@ int main() {
         g[a].push_back({b, c});
         inv[b].push_back({a, c});
     }
-    vector<long long> dist = dijkstra(g, s);
-    if (dist[t] == 1LL << 60) cout << -1 << endl;
-    else {
-        vector<pair<int, int>> ans;
-        int x = t;
-        while (x != s) {
-            for (auto e : inv[x]) {
-                if (dist[e.to] == dist[x] - e.cost) {
-                    ans.emplace_back(e.to, x);
-                    x = e.to;
-                    break;
-                }
-            }
-        }
-        reverse(ans.begin(), ans.end());
-        cout << dist[t] << ' ' << ans.size() << endl;
-        for (auto [u, v] : ans) {
-            cout << u << ' ' << v << endl;
-        }
+    vector<long long> dist = dijkstra(g, r);
+    for (int i = 0; i < n; i++) {
+        if (dist[i] == 1LL << 60) cout << "INF" << endl;
+        else cout << dist[i] << endl;
     }
 }
