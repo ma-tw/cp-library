a3c249de51b31fb0f0dd6a36b0775d53446ac29a
ma_tw <example@example.com>
2024-04-20 19:29:13 +0900

    fix dijkstra

--- test/dijkstra.test.cpp
+++ test/dijkstra.test.cpp
@@ -10,8 +10,8 @@ int main() {
     for (int i = 0; i < m; i++) {
         int a, b, c;
         cin >> a >> b >> c;
-        g[a].emplace_back(b, c);
-        inv[b].emplace_back(a, c);
+        g[a].push_back({b, c});
+        inv[b].push_back({a, c});
     }
     vector<long long> dist = dijkstra(g, s);
     if (dist[t] == 1LL << 60) cout << -1 << endl;
