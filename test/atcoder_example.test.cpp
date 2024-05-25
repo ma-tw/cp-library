26c31b5044ccbf8a8e4b298875df2252bb4ae267
ma_tw <someone@example.com>
2024-05-25 15:50:53 +0900

    atc ex

---
+++ test/atcoder_example.test.cpp
@@ -0,0 +1,10 @@
+#define PROBLEM "https://atcoder.jp/contests/abc333/tasks/abc333_a"
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) cout << n;
+    cout << endl;
+}
