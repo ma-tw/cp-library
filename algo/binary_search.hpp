7e50892e571dacd7fa88cba4e9f210bcedc5a32e
ma_tw <someone@example.com>
2024-06-15 20:04:59 +0900

    bin desktop

--- algo/binary_search.hpp
+++ algo/binary_search.hpp
@@ -1,8 +1,8 @@
 #include <functional>
 
 // binary search, finds the border of determ
-template <typename T = long long>
-T find_border(T ok, T ng, const std::function<bool(T)> determ) {
+template <typename T = long long, typename F>
+T find_border(T ok, T ng, const F determ) {
     while (abs(ok - ng) > 1) {
         T mid = (ok + ng) / 2;
         if (determ(mid)) {
